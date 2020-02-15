84226c66e41fe9ea19549f54ac3097595e427dfb
Rohit Yadav <rohitrao@cdot.in>
2020-02-15 08:40:03 +0530

    part 7

--- app/src/main/java/com/example/myquiz/QuestionActivity.java
+++ app/src/main/java/com/example/myquiz/QuestionActivity.java
@@ -24,6 +24,7 @@ public class QuestionActivity extends AppCompatActivity implements View.OnClickL
     private List<Question> questionList;
     private int quesNum;
     private CountDownTimer countDown;
+    private int score;
 
 
     @Override
@@ -46,6 +47,8 @@ public class QuestionActivity extends AppCompatActivity implements View.OnClickL
         option4.setOnClickListener(this);
 
         getQuestionsList();
+
+        score = 0;
 
     }
 
@@ -144,6 +147,7 @@ public class QuestionActivity extends AppCompatActivity implements View.OnClickL
         {
             //Right Answer
             ((Button)view).setBackgroundTintList(ColorStateList.valueOf(Color.GREEN));
+            score++;
 
         }
         else
@@ -205,6 +209,7 @@ public class QuestionActivity extends AppCompatActivity implements View.OnClickL
         {
             // Go to Score Activity
             Intent intent = new Intent(QuestionActivity.this,ScoreActivity.class);
+            intent.putExtra("SCORE", String.valueOf(score) + "/" + String.valueOf(questionList.size()));
             startActivity(intent);
             QuestionActivity.this.finish();
         }
--- app/src/main/java/com/example/myquiz/ScoreActivity.java
+++ app/src/main/java/com/example/myquiz/ScoreActivity.java
@@ -1,13 +1,40 @@
 package com.example.myquiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ScoreActivity extends AppCompatActivity {
+
+    private TextView score;
+    private Button done;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_score);
+
+        score = findViewById(R.id.sa_score);
+        done = findViewById(R.id.sa_done);
+
+        String score_str = getIntent().getStringExtra("SCORE");
+        score.setText(score_str);
+
+
+        done.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(ScoreActivity.this,MainActivity.class);
+                ScoreActivity.this.startActivity(intent);
+                ScoreActivity.this.finish();
+
+
+            }
+        });
+
     }
 }
